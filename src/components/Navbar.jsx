5f4d4a72401932b014ad676ae55442cad49a7bbe
matifelitti <matiasfelitti@gmail.com>
2025-07-01 21:13:21 -0300

    add cart.svg, change navbar jsx and css for cart image

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,47 +20,55 @@ function Navbar({ onCategoryChange }) {
         <div className="logo" onClick={() => navigate("/")}>
           MyStore
         </div>
+
+        <div className={`links ${menuOpen ? "active" : ""}`}>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              handleClick("all");
+            }}
+          >
+            All Products
+          </a>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              handleClick("footwear");
+            }}
+          >
+            Footwear
+          </a>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              handleClick("apparel");
+            }}
+          >
+            Apparel
+          </a>
+          <a
+            href="#"
+            onClick={(e) => {
+              e.preventDefault();
+              handleClick("accessories");
+            }}
+          >
+            Accessories
+          </a>
+          <img
+            src="/images/cart.svg"
+            alt="Cart"
+            className="cart-icon"
+            onClick={() => navigate("/cart")}
+          />
+        </div>
+
         <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
           ☰
         </button>
-      </div>
-      <div className={`links ${menuOpen ? "active" : ""}`}>
-        <a
-          href="#"
-          onClick={(e) => {
-            e.preventDefault();
-            handleClick("all");
-          }}
-        >
-          All Products
-        </a>
-        <a
-          href="#"
-          onClick={(e) => {
-            e.preventDefault();
-            handleClick("footwear");
-          }}
-        >
-          Footwear
-        </a>
-        <a
-          href="#"
-          onClick={(e) => {
-            e.preventDefault();
-            handleClick("apparel");
-          }}
-        >
-          Apparel
-        </a>
-        <a
-          href="#"
-          onClick={(e) => {
-            e.preventDefault();
-            handleClick("accessories");
-          }}
-        >
-          Accessories
-        </a>
       </div>
     </div>
   );
